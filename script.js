a20ccb9fc4606d449eece1fcc69d5ecad5d89a8c
beanbean <gracefernandez00@icloud.com>
2024-01-22 19:51:39 -0700

    added starter code

---
+++ script.js
@@ -0,0 +1,48 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Character Arrays (4)
+var specials = "!@#$%&*-+".split("")
+var numbers = "1234567890" .split("")
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// User Prompt questions: which characters to use?
+var isSpecials = confirm("Do you want a special character?")
+var isNumbers = confirm("Do you want numbers?")
+var isLowercase = confirm("Do you want lowercase letters?")
+var isUppercase = confirm("Do you want uppercase letters?")
+
+//conditions
+
+if (isSpecials) {
+var randomCharacter = Math.floor(Math.random()*9)
+  finalPass.push(specials[randomCharacter])
+}
+
+if (isNumbers) {
+var randomCharacter = Math.floor(Math.random()*10)
+  finalPass.push(numbers[randomCharacter])
+}
+
+if (isLowercase) {
+var randomCharacter = Math.floor(Math.random()*26)
+  finalPass.push(lowercase[randomCharacter])
+}
+
+if (isUppercase) {
+var randomCharacter = Math.floor(Math.random()*26)
+  finalPass.push(uppercase[randomCharacter])
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
